553c4fe029fd09de217259125c68c7b6c6fc8769
minoru-yamanaka <minoruyamanaka@icloud.com>
2024-04-12 17:16:37 -0300

    up120424-1716

--- Minoru_Avaliação Javascript/Atividades Práticas/Atividade 3 - 2pts/js/script.js
+++ Minoru_Avaliação Javascript/Atividades Práticas/Atividade 3 - 2pts/js/script.js
@@ -1,14 +1,7 @@
-
-
 function Funcao_Somar(){
 let var_numero1 = Number(numero1.value)
 let var_numero2 = Number(numero2.value)
 let var_soma = var_numero1 + var_numero2
 
 resposta1.innerText= var_soma
-
-}
-
-function Funcao_BackgroudColor(){
-    
 }
